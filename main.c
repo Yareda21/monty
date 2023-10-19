5865bea9c68855a3615fd945053268485484a00c
Yareda21 <ykebede39@yahoo.com>
2023-10-19 10:30:56 -0700

    a

--- main.c
+++ main.c
@@ -1,89 +1,87 @@
 #include "monty.h"
-#define _GNU_SOURCE
+stack_t *head = NULL;
 
-#include <stdio.h>
-#include <stdlib.h>
-
-bus_t bus = {NULL, NULL, NULL, 0};
-
-ssize_t my_getline(char **content, size_t *size, FILE *file) {
-    char *buffer;
-    size_t capacity = 50;
-    size_t length = 0;
-    int character;
-    
-    buffer = malloc(capacity * sizeof(char));
-    if (!buffer) return -1;
-
-    while ((character = fgetc(file)) != EOF && character != '\n') {
-        if (length + 1 >= capacity) {
-            capacity *= 2;
-            buffer = realloc(buffer, capacity * sizeof(char));
-            if (!buffer) return -1;
-        }
-        buffer[length++] = character;
-    }
-
-    if (length == 0 && character == EOF) {
-        free(buffer);
-        return -1;
-    }
-
-    buffer[length++] = '\0';
-    *content = buffer;
-    *size = length;
-
-    return length - 1;
-}
-
-
+void free_nodes(void);
 
 /**
- * main - monty code interpreter
- * @argc: number of arguments
- * @argv: monty file location
- *
- * Return: 0 on success
+ * create_node - Creates a node.
+ * @n: Number to go inside the node.
+ * Return: Upon sucess a pointer to the node. Otherwise NULL.
  */
+stack_t *create_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+
+/**
+ * main - entry point
+ * @argc: arguments count
+ * @argv: list of arguments
+ * Return: always 0
+ */
+
 int main(int argc, char *argv[])
 {
-	FILE *file;
-	stack_t *stack = NULL;
-	char *content = NULL;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	unsigned int counter = 0;
-
-	/* Check if the number of arguments is correct */
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
-	/* Open the file */
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-
-	/* Read and execute each line in the file */
-	while ((read_line = my_getline(&content, &size, file)) > 0)
-	{
-		bus.content = content;
-		counter++;
-		execute(content, &stack, counter, file);
-		free(content);
-		content = NULL;
-	}
-
-	/* Clean up */
-	free_stack(stack);
-	fclose(file);
-
+	open_file(argv[1]);
+	free_nodes();
 	return (0);
+}
+
+
+
+/**
+ * add_to_queue - Adds a node to the queue.
+ * @new_node: Pointer to the new node.
+ * @ln: line number of the opcode.
+ */
+void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+
+}
+
+/**
+ * free_nodes - Frees nodes in the stack.
+ */
+void free_nodes(void)
+{
+	stack_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
 }
 
